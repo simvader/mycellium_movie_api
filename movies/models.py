8abc2e7b4a53a48abe773fbea6946ef6a2bd7311
simvader <simbadfernandes@gmail.com>
2022-12-05 10:25:28 -0300

    movie starter models && django_rest_framework added

--- movies/models.py
+++ movies/models.py
@@ -1,13 +1,49 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Continent(models.Model):
+    name = models.CharField(max_length=60)
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+
+
+
+class Director(models.Model):
+    profile = models.ForeignKey('Profile', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.profile.get_full_name()
+
+
+class Actor(models.Model):
+    profile = models.ForeignKey('Profile', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+
 class Movie(models.Model):
     imdb = models.CharField(max_length=50)
     title = models.CharField(max_length=100)
     year = models.IntegerField()
-
-class Director(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    directors = models.ManyToManyField(Director)
+    actors = models.ManyToManyField(Actor)
 
     def __str__(self):
-        return User.get_full_name()
+        return f'{self.title} - {self.year}'
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
+    seen_movies = models.ManyToManyField('Movie', through= 'MovieProfile')
+
+    def __str__(self):
+        return self.user.get_full_name()
+
+class MovieProfile(models.Model):
+    profile = models.ForeignKey(Profile, models.PROTECT)
+    movie = models.ForeignKey(Movie, on_delete=models.PROTECT)
