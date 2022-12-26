01fd1e92f8d7b9f2366da09668a19b1779e77160
Simbad Fernandes <simbad@Simbads-MacBook-Pro.local>
2022-12-26 20:26:22 -0300

    post

--- mycelliumMovieApi/settings.py
+++ mycelliumMovieApi/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'mycelliumMovieApi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mycellium_movies',
-        'USER': 'root',
-        'PASSWORD': 'jajaime89',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
